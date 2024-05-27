1d1f9366055d3495c3b8cee2a5f06de56e495615
Daniel Breitschaft <danielchaft@outlook.com>
2024-05-27 16:55:55 -0300

    Mensagem dentro da funcao removida

--- 7D-GdScript/level.gd
+++ 7D-GdScript/level.gd
@@ -1,9 +1,6 @@
 #Herda de Node (Tipo do objeto)
 extends Node
 
-#Função executada assim que o objeto ficar pronto, para rodar a cena, vá até o
-#caminho dela -> aprenda_gdscript -> dia_1 -> conceito -> dia_1.tscn (clique duplo para abrir)
-#Em seguida, pressione F6 para rodar a cena que foi aberta.
 func _ready() -> void:
 
 	print("Hello World!")
