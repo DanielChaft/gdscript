8c0b8eaf921406c6387445364ff034deb22cf966
Daniel Breitschaft <danielchaft@outlook.com>
2024-05-27 16:51:06 -0300

    Mensagem dentro da funcao removida

--- 7D-GdScript/level.gd
+++ 7D-GdScript/level.gd
@@ -5,7 +5,5 @@ extends Node
 #caminho dela -> aprenda_gdscript -> dia_1 -> conceito -> dia_1.tscn (clique duplo para abrir)
 #Em seguida, pressione F6 para rodar a cena que foi aberta.
 func _ready() -> void:
-	
-	#Mensagem que irá aparecer no console (output), encontrado no canto inferior
-	#esquerdo
+
 	print("Hello World!")
